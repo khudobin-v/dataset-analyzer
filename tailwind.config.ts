56de0d7e6a829541c13fd123102c551eaef8c0e6
Василий Худобин (MacOS) <khudobin_v@icloud.com>
2024-07-01 02:42:56 +0300

    Работа с цветами

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,9 @@ const config = {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
       colors: {
+        "red-color": "#BD4C4C",
+        "green-color": "#4CBD87",
+        "blue-color": "#4C94BD",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
