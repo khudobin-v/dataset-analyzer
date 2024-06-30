1504a72fa644a75f9e2484bcffdd5648542b5407
Василий Худобин (MacOS) <khudobin_v@icloud.com>
2024-07-01 02:56:34 +0300

    Правки для компонента кнопки

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,7 +12,7 @@ export const Button = ({ Icon, color, children }: ButtonProps) => {
   return (
     <button
       className={cn(
-        "hover:opacity/90 flex items-center gap-3 rounded-md bg-black/80 p-2 px-3 text-sm font-medium text-white transition-colors",
+        "flex items-center gap-3 rounded-md bg-black/80 p-2 px-3 text-sm font-medium text-white transition-colors hover:opacity-90",
         { "bg-red-color": color === "red" },
         { "bg-green-color": color === "green" },
         { "bg-blue-color": color === "blue" },
