065fe176ce369a5112937a350a1839b172b3d904
Василий Худобин (MacOS) <khudobin_v@icloud.com>
2024-07-01 03:09:16 +0300

    Добавление отображения названия файла и функционала очистки поля ввода

--- src/components/ui/file-input.tsx
+++ src/components/ui/file-input.tsx
@@ -1,6 +1,24 @@
-import { FolderIcon } from "lucide-react";
+"use client";
+
+import { FolderIcon, XIcon } from "lucide-react";
+import { useRef, useState } from "react";
 
 export const FileInput = () => {
+  const [fileName, setFileName] = useState<string>("");
+  const handleFileChange = (e: any) => {
+    if (e.target.files.length > 0) {
+      setFileName(e.target.files[0].name);
+    } else {
+      setFileName("");
+    }
+  };
+  const fileInputRef = useRef(null);
+  const handleClearFile = () => {
+    setFileName("");
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
   return (
     <div className="flex w-full items-center space-x-2">
       <label
@@ -8,11 +26,20 @@ export const FileInput = () => {
         className="flex w-full cursor-pointer items-center gap-2.5 rounded-md border-[1.5px] border-gray-300 px-2.5 py-3 outline-none transition-colors hover:border-black/10 hover:bg-black/5"
       >
         <FolderIcon size={18} className="opacity-60" />
-        <span className="text-sm font-medium text-black/60">
-          Выберите путь до папки с датасетом
+        <span className="flex w-full justify-between text-sm font-medium text-black/60">
+          {fileName ? fileName : "Выберите путь до папки с датасетом"}
+          <span>
+            {fileName && <XIcon size={20} onClick={handleClearFile} />}
+          </span>
         </span>
       </label>
-      <input id="file-input" type="file" className="hidden" />
+      <input
+        id="file-input"
+        type="file"
+        className="hidden"
+        onChange={handleFileChange}
+        ref={fileInputRef}
+      />
     </div>
   );
 };
